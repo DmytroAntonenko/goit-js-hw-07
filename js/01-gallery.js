fda726ce6c72ecbc1b324b2160cdc874835a1270
DmytroAntonenko <d.antonenko81@gmail.com>
2022-10-05 11:12:51 +0300

    #1

---
+++ js/01-gallery.js
@@ -0,0 +1,52 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const imagesContainer = document.querySelector('.gallery');
+
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+imagesContainer.addEventListener('click', onImagesContainerClick);
+
+
+function createImageCardsMarkup (galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href='${preview}'>
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source="${original}"
+      alt="${description}"
+    /> 
+  </a>
+</div>
+`;
+}).join('');
+}
+
+function onImagesContainerClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+   
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" width="1280">`
+    );
+    
+    instance.show();
+
+    window.addEventListener('keydown', onEscPress);
+
+    function onEscPress(event) {
+        
+        if (event.code === 'Escape') {
+          instance.close()
+        }
+    }
+}
